1468c08f00d7a3ce3b3f72517368d31efa541241
SaraToth <saratoth101@gmail.com>
2025-04-02 20:12:21 +0900

    Feat: Create Card component that renders Pokemon
    Card component takes Pokemon API data passed in as a prop, and returns a grid card of that pokemon's info and image.

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+/**
+ * Creates a pokemon card that renders on board component page
+ * @module card
+ * @returns {HTMLElement} Div - pokemon UI card with pokemon name, id and image 
+ */
+
+export default function Card({pokemonObject}) {
+    return (
+        <div className="card">
+            <div>
+                <h2>{pokemonObject.name}</h2>
+                <p>{pokemonObject.pokeId}</p>
+            </div>
+            <img src={pokemonObject.imgUrl} />
+        </div>
+    )
+}
