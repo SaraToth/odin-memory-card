001f4c735472836c0b9f1ec5990327b5bfbf85e2
SaraToth <saratoth101@gmail.com>
2025-04-02 20:09:32 +0900

    Feat: Board component and API fetch request
    Create board component page and useEffect to process API call to the pokemonAPI to retreive relevant pokemon information and pictures

---
+++ src/components/Board.jsx
@@ -0,0 +1,63 @@
+/** 
+ * @module board 
+ * Board component acts as a parent and manages all other components
+ */
+
+import { useState } from "react";
+import { useEffect } from "react";
+import Card from "./Card";
+
+export default function Board() {
+
+    const [pokemonData, setPokemonData] = useState([]);
+
+    /**
+     * API fetch call that updates pokemonData state with filtered API data
+     */
+    useEffect(() => {
+        // 12 Pokemon chosen for gameplay
+        const pokemonNames = ["pikachu", "squirtle", "bulbasaur", 
+            "charmander", "vulpix", "jigglypuff", 
+            "staryu", "onix", "piplup", 
+            "togepi", "eevee", "shaymin-land"]; 
+        
+        /**
+         * Helper async function to fetch relevant pokemon data from API
+         * @param {String} pokemonName 
+         * @returns {Object} Pokemon object with properties: name id and imgUrl for that pokemon
+         */
+        async function fetchPokemonData(pokemonName) {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+            const pokemonData = await response.json();
+
+            return {
+                name: pokemonName,
+                pokeId: pokemonData.id,
+                imgUrl: pokemonData.sprites.other["official-artwork"].front_default,
+            }
+        }
+
+        /**
+         * Fetches all filtered pokemon data from API and updates component state with results.
+         */
+        async function fetchAllPokemon() {
+            const results = await Promise.all(pokemonNames.map(pokemon => fetchPokemonData(pokemon)));
+            setPokemonData(results);
+            }
+        
+        fetchAllPokemon();
+    }, []);
+    
+    console.log(pokemonData); // Test to confirm that useEffect sets state correctly.
+
+    return (
+        <div>
+            <h1>Pokemon!</h1>
+            <div className="grid-parent">
+                {pokemonData && pokemonData.map((pokemon)=> 
+                    <div key={pokemon.pokeId}><Card pokemonObject={pokemon} /></div>
+                )}
+            </div>
+        </div> 
+    )
+}
