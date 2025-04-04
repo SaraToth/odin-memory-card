6a3dc703d904dae9fcfcf1c178107e4087428d1e
SaraToth <saratoth101@gmail.com>
2025-04-04 17:49:55 +0900

    Docs: Update jsdoc for Modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,16 @@
 import "../styles/Modal.css"
 
+/**
+ * Modal Component
+ * 
+ * @typedef {object} ModalProps
+ * @property {number} bestScore The player's best score so far
+ * @property {number} currentScore The current score in the ongoing game
+ * @property {() => void} onClick Event handler for button clicks
+ * 
+ * @param {ModalProps} props
+ * @returns {JSX.Element} The rendered modal component
+ */
 export default function Modal({currentScore, bestScore, onClick}) {
     return (
         <div className="modal">
